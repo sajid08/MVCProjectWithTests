a6ced06575734e779439be5dc16c94c11242a6af
sajid <sajid@DESKTOP-GDPFL4R>
2016-11-18 00:01:54 +0000

    Add project files.

---
+++ MVCProjectWithTests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProjectWithTests.Startup))]
+namespace MVCProjectWithTests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
